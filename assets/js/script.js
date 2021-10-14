d7d2a379da5603bb4c63c072f6c7477e1f34b0b9
lilys0 <lsouryaphanh@gmail.com>
2021-10-14 17:01:45 +1100

    digital services update

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// When user clicks on Digital Marketing Services button then a marketing services section will be unhidden.
+
+$("#marketing-services").click(() => {
+  $(".marketing").toggle("hidden");
+});
